ed1f7fb82fd56c17eb63dce62fec2dd49263ac58
mia-rune <maurine.d35@gmail.com>
2025-11-12 15:04:29 +0100

    dotenv

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-require('dotenv').config();
 const express = require('express');
+require('dotenv').config(); //process.env.NOMDELAVARIABLE pour appeler une variable
 const session = require('express-session');
 const path = require('path');
 
